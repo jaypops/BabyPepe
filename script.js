5e0eada3e02e3f3ca2680bf87ee9ffe71880173e
Timothy-TO <73559338+jaypops@users.noreply.github.com>
2023-06-16 11:58:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -93,13 +93,17 @@ var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: "auto",
+  slidesPerView: 3,
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
     depth: 100,
     modifier: 1,
-    slideShadows: true,
+    slideShadows: false,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
   },
   pagination: {
     el: ".swiper-pagination",
