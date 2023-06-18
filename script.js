3d549a3e0359122cb9bb81e3abf9980a010cc3f7
Timothy-TO <73559338+jaypops@users.noreply.github.com>
2023-06-18 17:23:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: 3,
+  slidesPerView: 4,
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
